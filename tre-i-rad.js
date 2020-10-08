1b0341c5761d14847d20ab04868e5200be05b874
moapng <larssonmoaml@gmail.com>
2020-10-08 21:39:10 +0200

    game-over function now works

--- tre-i-rad.js
+++ tre-i-rad.js
@@ -4,22 +4,26 @@ function markSquare(id) {
     var elem = document.getElementById(id);
     if (elem.innerHTML != "") return;
     elem.innerHTML = marker;
+
     if (marker == "X") marker = "O";
     else marker = "X";
+
+
+    var isAllFull = true;
+    var elems = document.getElementsByClassName("square");
+    for (e of elems) {
+        if (e.innerHTML === "") isAllFull = false;
+    }
+    if (isAllFull) {
+        document.getElementById("message").innerHTML = "GAME OVER";
+    } else {
+        document.getElementById("message").innerHTML = "Fortsätt spela!"
+    }
 }
 
 function resetGame() {
     var elem = document.getElementsByClassName("square");
     for (let element of elem) {
         element.innerHTML = "";
-    }
-}
-
-function gameOver() {
-    var elem = document.getElementsByClassName("square");
-    for (let element of elem) {
-        if (elem.innerHTML != "") {
-            document.getElementById("game-over").innerHTML = "GAME OVER";
-        }
     }
 }
